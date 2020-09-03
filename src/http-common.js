b1a127fe58d2c1c92e514196232582a22a9e48df
André Luiz Moreno <32988439+konfigbr@users.noreply.github.com>
2020-09-03 07:07:46 -0300

    Update http-common.js
    Porta 8081

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://api-grades-almoreno.herokuapp.com/',
+  baseURL: 'https://api-grades-almoreno.herokuapp.com:8081/',
   headers: {
     'Content-type': 'application/json',
   },
