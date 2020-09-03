17be79071b917f82c6d122375786229a5055335a
André Luiz Moreno <32988439+konfigbr@users.noreply.github.com>
2020-09-03 07:11:48 -0300

    Update http-common.js
    retirando porta

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://api-grades-almoreno.herokuapp.com:8081/',
+  baseURL: 'https://api-grades-almoreno.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
